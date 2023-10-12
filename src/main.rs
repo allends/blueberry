7852360da8a933b9f75e0c24f6aa44620d36439c
allends <allen.davisswing@rutgers.edu>
2023-10-11 23:22:54 -0400

    test

--- src/main.rs
+++ src/main.rs
@@ -103,6 +103,7 @@ impl Router {
         for route in &self.routes {
             if routes_match(path, route.path.as_str()) && method == route.method {
                 let params = get_params(path, &route.path);
+                println!("{} to {}", path, route.path);
                 (route.handler)(stream, request, route.state.clone(), params);
                 return;
             }
